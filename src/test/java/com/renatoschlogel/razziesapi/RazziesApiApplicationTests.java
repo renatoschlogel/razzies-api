78d2be91bb6919cdc8788ad4976d37ab454ce836
Renato W Schlogel <renato.s@outlook.com>
2025-07-20 22:47:08 -0300

    Estrutura inicial do Projeto.

---
+++ src/test/java/com/renatoschlogel/razziesapi/RazziesApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.renatoschlogel.razziesapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RazziesApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
