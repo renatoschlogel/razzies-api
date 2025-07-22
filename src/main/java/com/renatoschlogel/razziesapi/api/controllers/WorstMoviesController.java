925989ea8523bab0eeaa4bbe5745deb1182ef96e
Renato W Schlogel <renato.s@outlook.com>
2025-07-22 01:38:07 -0300

    Documentando api com Swagger.

--- src/main/java/com/renatoschlogel/razziesapi/api/controllers/WorstMoviesController.java
+++ src/main/java/com/renatoschlogel/razziesapi/api/controllers/WorstMoviesController.java
@@ -5,6 +5,10 @@ import com.renatoschlogel.razziesapi.api.dtos.WorstMovieProducersIntervalsRespon
 import com.renatoschlogel.razziesapi.domain.model.AwardIntervalsResult;
 import com.renatoschlogel.razziesapi.domain.model.ProducerInterval;
 import com.renatoschlogel.razziesapi.domain.usecases.GetProducersWithAwardIntervals;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,7 +16,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
-
 @AllArgsConstructor
 @RestController
 @RequestMapping("/api/v1/worst-movies")
@@ -20,6 +23,15 @@ public class WorstMoviesController {
 
     private final GetProducersWithAwardIntervals getProducersWithAwardIntervals;
 
+    @Operation(
+            summary = "Get Award Intervals for Producers",
+            description = "Returns a list of producers with the longest and shortest intervals between two consecutive awards."
+    )
+    @ApiResponse(responseCode = "200", description = "Successfully retrieved the list of producer intervals.",
+            content = { @Content(mediaType = "application/json",
+                    schema = @Schema(implementation = WorstMovieProducersIntervalsResponseDto.class)) })
+    @ApiResponse(responseCode = "500", description = "Internal server error. Failed to process the request.",
+            content = @Content)
     @GetMapping("/producers-intervals")
     public ResponseEntity<WorstMovieProducersIntervalsResponseDto> producersAwardsIntervals() {
         AwardIntervalsResult awardIntervalsResult = getProducersWithAwardIntervals.execute();
---
+++ src/main/java/com/renatoschlogel/razziesapi/config/SwaggerOpenApiConfig.java
@@ -0,0 +1,18 @@
+package com.renatoschlogel.razziesapi.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Golden Raspberry Awards API",
+                version = "1.0",
+                description = "API for querying winners of the Golden Raspberry Awards.",
+                contact = @Contact(name = "Renato Welinton Schlogel", email = "renato.s@outlook.com")
+        )
+)
+public class SwaggerOpenApiConfig {
+}
